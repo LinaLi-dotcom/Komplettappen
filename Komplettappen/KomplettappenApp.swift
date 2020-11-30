914d2b36d4a7667254879c07ca2c683869470ca7
lina <linasigridli@gmail.com>
2020-11-30 12:39:53 +0100

    Initial Commit

---
+++ Komplettappen/KomplettappenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomplettappenApp.swift
+//  Komplettappen
+//
+//  Created by Lina Li on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct KomplettappenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
